4a2cec271f35598724daa761b401a68c992baae4
Garika Sreekanth <sreekanthgarika91@gmail.com>
2020-12-27 18:20:05 +0530

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Garika Sreekanth on 27/12/20.
+//  Copyright © 2020 Garika Sreekanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
